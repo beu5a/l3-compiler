e8707ebe8b522d5645f1aeecde2888bc54bf9e50
habib <hassan.habib@epfl.ch>
2024-03-02 15:04:35 +0100

    Initial commit

---
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -0,0 +1,23 @@
+package l3
+
+import l3.{SymbolicCL3TreeModule => S}
+import l3.{HighCPSTreeModule => H}
+
+object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
+  def apply(tree: S.Tree): H.Tree =
+    transform(tree)(_ => H.Halt(IntLit(L3Int(0))))
+
+  private def transform(tree : S.Tree)(ctx: H.Atom => H.Tree): H.Tree = {
+    given Position = tree.pos
+
+    tree match {
+      case S.Lit(value) => 
+        ctx(value)
+      case S.Ident(name) =>  
+        ctx(name)
+    }
+  }
+    
+
+
+}
