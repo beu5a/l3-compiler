dc638d86c3928a3e741a863530cea23a992e64f6
Mohamed Yassine Boukhari <mohamed.boukhari@epfl.ch>
2024-03-10 14:47:17 +0100

    ass2 part1 pass all tests

--- compiler/src/l3/CL3ToCPSTranslator.scala
+++ compiler/src/l3/CL3ToCPSTranslator.scala
@@ -1,16 +1,33 @@
 package l3
 
-import l3.{SymbolicCL3TreeModule => S}
+import l3.{CL3Literal => CL3Lit}
 import l3.{HighCPSTreeModule => H}
+import l3.{L3Primitive => L3Prim}
+import l3.{SymbolicCL3TreeModule => S}
+
+/**
+ * A translator from CL₃ to CPS.
+ */
+
+
+ 
+
+/**
+  * TODO : refractor name generation
+  * TODO : refractor some variable namings
+  * TODO : rename context curry to transform curry maybe ? add reverse args
+  * 
+  */
+
 
 object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
 
-  val l3true = S.Lit(BooleanLit(true))
-  val l3false = S.Lit(BooleanLit(false))
+
   
   def apply(tree: S.Tree): H.Tree =
-    transform(tree)(_ => H.Halt(IntLit(L3Int(0))))
-  
+    transform(tree)(_ => H.Halt(CL3Lit.IntLit(L3Int(0))))
+
+  // Maybe change name to curryTransform , and use foldLeft?
   def curryContext(trees: Seq[S.Tree], accContext: Seq[H.Atom])(ctx: Seq[H.Atom] => H.Tree): H.Tree =
     trees match {
       case Seq() => ctx(accContext)
@@ -23,6 +40,12 @@ object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
   private def transform(tree : S.Tree)(ctx: H.Atom => H.Tree): H.Tree = {
     given Position = tree.pos
 
+    //define here, literals need implicit position
+    val l3true = S.Lit(CL3Lit.BooleanLit(true))
+    val l3false = S.Lit(CL3Lit.BooleanLit(false))
+    val l3Id = L3Prim.Id
+    val l3Eq = L3Prim.Eq
+
     tree match {
       case S.Lit(value) => 
         ctx(value)
@@ -31,38 +54,58 @@ object CL3ToCPSTranslator extends (S.Tree => H.Tree) {
       case S.Let(Seq(),e) => 
         transform(e)(ctx)
       case S.Let(Seq((n1, e1), niei @_*), e) =>
-        transform(e1)(a1 => H.LetP(n1, L3Id, Seq(a1), transform(S.Let(niei, e))(ctx)))
+        transform(e1)(a1 => H.LetP(n1, l3Id, Seq(a1), transform(S.Let(niei, e))(ctx)))
       case S.LetRec(fs, e) =>
-        val cps_fs = fs map {
-          val retC = H.Name.fresh("ret_c")
-          H.Fun(_.name, retC, _.args, transform(_.body)(a => H.AppC(retC, Seq(a))))
-        }
+        val cps_fs = fs map (f => 
+          val retC = Symbol.fresh("ret_c")
+          H.Fun(f.name, retC, f.args, transform(f.body)(a => H.AppC(retC, Seq(a))))
+      )
         H.LetF(cps_fs, transform(e)(ctx))
-      case S.App(f, args) =>
-        
-        val retCntName = H.Name.fresh("ret_c")
-        val retCntArgName = H.Name.fresh("v")
-        val appF = (as: Seq[Atom]) => H.AppF(f, retCntName, as)
 
-        val initCtx = (as : Seq[H.Atom]) => {
-          val retCnt = H.Cnt(retCntName, Seq(retCntArgName), ctx(retCntArgName))
-          H.LetC(Seq(retCnt), appF(as))
-        }
+      case S.App(f, args) =>
+        val retCntName = Symbol.fresh("ret_c")
+        val retCntArgName = Symbol.fresh("v")
+        val appF = (a: H.Atom, as: Seq[H.Atom]) => H.AppF(a, retCntName, as)
+        val retCnt = H.Cnt(retCntName, Seq(retCntArgName), ctx(retCntArgName))
+
+        val initCtx = (as : Seq[H.Atom]) => 
+          H.LetC(Seq(retCnt), transform(f)(a => appF(a, as)))
+      
         curryContext(args, Seq())(as => initCtx(as)) //Do we need to reverse the args?
+
+      
+      case S.If(S.Prim(p:L3TestPrimitive, args), e2, e3) =>
+        val ifCntName = Symbol.fresh("if_c")
+        val ifCntArgName = Symbol.fresh("r")
+        val thenCntName = Symbol.fresh("then_c")
+        val elseCntName = Symbol.fresh("else_c")
+
+        val ifCnt = H.Cnt(ifCntName, Seq(ifCntArgName), ctx(ifCntArgName))
+        val thenCnt = H.Cnt(thenCntName, Seq(), transform(e2)(a2 => H.AppC(ifCntName, Seq(a2))))
+        val elseCnt = H.Cnt(elseCntName, Seq(), transform(e3)(a3 => H.AppC(ifCntName, Seq(a3))))
+
+        val letCBody = curryContext(args, Seq())(as => H.If(p, as, thenCntName, elseCntName))
+        H.LetC(Seq(ifCnt, thenCnt, elseCnt), letCBody)
        
       case S.If(e1, e2, e3) =>
         transform(
-          S.If(S.Prim(L3Eq, Seq(e1, l3false)), e3, e2)
+          S.If(
+            S.Prim(l3Eq, Seq(e1, l3false)), e3, e2)
           )
           (ctx)
-      case p @ S.Prim(p:L3TestPrimitive , ei) =>
-        transform(
-          S.If(p,l3true,l3false)
-        )
-        (ctx)
-      
-      case _ @ S.Prim(p:L3ValuePrimitive, args) =>
 
+      case tprim @ S.Prim(p:L3TestPrimitive , ei) =>
+        transform(S.If(tprim,l3true,l3false))(ctx)
+      
+      case S.Prim(p:L3ValuePrimitive, args) =>
+        val primName = Symbol.fresh("value_p")
+        val initCtx = (as: Seq[H.Atom]) => H.LetP(primName, p, as, ctx(primName))
+        curryContext(args, Seq())(as => initCtx(as))
+
+      case S.Halt(arg) =>
+        transform(arg)(H.Halt(_))
+      
+      case _ => sys.error("Not implemented yet")
         
 
 
